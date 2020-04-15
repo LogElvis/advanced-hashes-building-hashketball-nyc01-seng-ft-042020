adc6e2ab237b18d96f0195fd743fb0ed11212eaf
Elvis Ning <ningelvis@gmail.com>
2020-04-15 19:35:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -20,7 +20,7 @@ def game_hash
          :player_name => "Reggie Evans",
          :number => 30,
          :shoe => 14,
-         :Points => 12,
+         :points => 12,
          :rebounds => 12,
          :assists => 12,
          :steals => 12,
@@ -67,26 +67,26 @@ def game_hash
       :colors => ["Turquoise", "Purple"],
       :players => [
         {
-          :player_name => "Jeff Adrien",
-          :Number => 4,
-          :Shoe => 18,
-          :Points => 10,
-          :Rebounds => 1,
-          :Assists => 1,
-          :Steals => 2,
-          :Blocks => 7,
-          :Slam_Dunks => 2
+         :player_name => "Jeff Adrien",
+         :number => 4,
+         :shoe => 18,
+         :points => 10,
+         :rebounds => 1,
+         :assists => 1,
+         :steals => 2,
+         :blocks => 7,
+         :slam_dunks => 2
         },
         {
-          :player_name => "Bismack Biyombo",
-          :Number => 0,
-          :Shoe => 16,
-          :Points => 12,
-          :Rebounds => 4,
-          :Assists => 7,
-          :Steals => 22,
-          :Blocks => 15,
-          :Slam_Dunks => 10
+         :player_name => "Bismack Biyombo",
+         :number => 0,
+         :shoe => 16,
+         :points => 12,
+         :rebounds => 4,
+         :assists => 7,
+         :steals => 22,
+         :blocks => 15,
+         :slam_dunks => 10
         },
         {
           :player_name => "DeSagna Diop",
